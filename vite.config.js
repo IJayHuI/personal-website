2631bde580ea1a67879b0a23ff722d502f9a6b6c
IJayHuI <2949763264@qq.com>
2025-05-22 02:35:23 +0800

    update: 项目基础配置

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/background': {
+        target: 'https://cn.bing.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/background/, '')
+      },
+      '/server': {
+        target: 'https://server.jayhu.site/api',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/server/, '')
+      }
+    },
+    host: '0.0.0.0'
+  }
 })
