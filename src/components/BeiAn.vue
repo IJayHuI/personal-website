fabb4c077292182c8822ed8c2c8d41741585c730
IJayHuI <2949763264@qq.com>
2025-05-28 02:26:20 +0800

    update: 备案信息组件

---
+++ src/components/BeiAn.vue
@@ -0,0 +1,11 @@
+<template>&copy; 2024-2025 JayHu.保留所有权利。<a target="_blank" href="https://beian.miit.gov.cn">浙ICP备2024102465号</a></template>
+
+<style scoped>
+  a {
+    color: unset;
+    text-decoration: none;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
