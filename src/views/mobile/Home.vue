a0a8d6b77f86d89adfa071532e6e238dcd5775d8
IJayHuI <2949763264@qq.com>
2025-05-29 20:19:16 +0800

    update: 移动端 Home 页

---
+++ src/views/mobile/Home.vue
@@ -0,0 +1,40 @@
+<script setup>
+  import { NLayout, NLayoutContent, NLayoutFooter } from 'naive-ui'
+  import BeiAn from '../../components/BeiAn.vue'
+  import { theme } from '../../main'
+
+  const lightThemeOverrides = {
+    Card: {
+      color: 'rgba(255, 255, 255, 0.5)',
+      borderRadius: '10px'
+    },
+    Layout: {
+      color: 'rgba(0, 0, 0, 0)',
+      footerColor: 'rgba(255, 255, 255, 0.4)',
+      textColor: '#000'
+    }
+  }
+
+  const darkThemeOverrides = {
+    Card: {
+      color: 'rgba(24, 24, 28, 0.5)',
+      borderRadius: '10px'
+    },
+    Layout: {
+      color: 'rgba(16, 16, 20, 0.4)',
+      footerColor: 'rgba(24, 24, 28, 0.4)'
+    }
+  }
+</script>
+<template>
+  <n-config-provider :theme="theme" :theme-overrides="theme === null ? lightThemeOverrides : darkThemeOverrides">
+    <n-layout position="absolute">
+      <n-layout-content position="absolute"></n-layout-content>
+      <n-layout-footer position="absolute">
+        <div style="text-align: center; margin: 5px">
+          <BeiAn />
+        </div>
+      </n-layout-footer>
+    </n-layout>
+  </n-config-provider>
+</template>
