1785d82f60256ad0d9faf277be37b54f884bcf5d
IJayHuI <2949763264@qq.com>
2025-08-09 10:08:10 +0800

    fix: 将方法导出

--- src/services/General.js
+++ src/services/General.js
@@ -1,6 +1,6 @@
 import { NIcon } from 'naive-ui'
 import { h } from 'vue'
 
-const renderIcon = (icon) => {
+export const renderIcon = (icon) => {
   return () => h(NIcon, null, { default: () => h(icon) })
 }
