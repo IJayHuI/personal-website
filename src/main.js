bd76893401b5b5eb16a4d5d83c9d704afbe979e8
IJayHuI <2949763264@qq.com>
2025-10-15 22:33:06 +0800

    update: 使用新后端，引入全局Supabase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import { createApp, ref, computed, watch } from 'vue'
 import '@/style.css'
 import App from '@/App.vue'
 
+// Supabase 后端
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://jspchtyovxlpzgbywifs.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcGNodHlvdnhscHpnYnl3aWZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1MTI1NzEsImV4cCI6MjA3NjA4ODU3MX0.E5HbRAUarb0s8nIK8zB0hO2L54XBjE9XlaiYtiGxzt0'
+export const supabase = createClient(supabaseUrl, supabaseKey)
+
 import router from '@/routes'
 import {
   create,
