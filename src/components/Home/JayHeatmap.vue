73f87121bba7b5a0f361a679cb8d945445dc55ac
IJayHuI <2949763264@qq.com>
2025-10-18 16:18:07 +0800

    fix: 2（完）

---
+++ src/components/Home/JayHeatmap.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import { onBeforeMount } from 'vue'
+  import { supabase } from '@/main'
+  import { NHeatmap } from 'naive-ui'
+  import { heatmap } from '@/services/Home'
+
+  onBeforeMount(async () => {
+    if (!heatmap.value.needGetData) return
+    const { data, error } = await supabase.functions.invoke('heatmap')
+    if (error) console.error(error)
+    else {
+      heatmap.value = {
+        loadingStatus: false,
+        totalContributions: data.totalContributions,
+        heatmapData: data.heatmapData,
+        needGetData: false
+      }
+    }
+  })
+  const props = defineProps({
+    cardSize: { type: String, required: false, default: 'medium' },
+    heatmapSize: { type: String, required: false, default: 'large' }
+  })
+</script>
+<template>
+  <n-card v-slide-in :title="`最近一年有${heatmap.totalContributions}次贡献`" class="interaction" :size="props.cardSize" @touchstart="">
+    <n-scrollbar x-scrollable class="max-w-full" content-class="flex justify-center">
+      <n-heatmap :data="heatmap.heatmapData" :loading-data="heatmap.heatmapData" :loading="heatmap.loadingStatus" :size="props.heatmapSize" :fill-calendar-leading="true" />
+    </n-scrollbar>
+  </n-card>
+</template>
