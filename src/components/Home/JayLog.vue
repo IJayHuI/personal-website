2d1a1fc494174045fea8428d4575de129888819b
IJayHuI <2949763264@qq.com>
2025-10-19 17:38:29 +0800

    chore: update app version to 5.2

--- src/components/Home/JayLog.vue
+++ src/components/Home/JayLog.vue
@@ -19,13 +19,13 @@
 </script>
 <template>
   <n-spin :show="log.loadingStatus">
-    <n-scrollbar class="max-h-60" content-class="grid gap-5">
+    <n-scrollbar class="max-h-60" content-class="grid gap-2">
       <n-button secondary style="word-wrap: break-word; white-space: normal" size="large" tag="a" target="_blank" href="https://github.com/IJayHuI/personal-website/releases/">前往 Github Realeases 查看更详细信息</n-button>
       <n-timeline>
         <n-timeline-item v-for="item in log.datas" :time="item.date">
           <template #default>
             <template v-for="content in item.content">
-              <p>· {{ content }}</p>
+              <p>- {{ content }}</p>
             </template>
           </template>
         </n-timeline-item>
