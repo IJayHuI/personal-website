a1982040dbf74257b3259d563a80191bc2f00ccd
IJayHuI <2949763264@qq.com>
2025-10-20 17:58:57 +0800

    update: remove proxy config

--- vite.config.js
+++ vite.config.js
@@ -11,18 +11,6 @@ export default defineConfig({
     __APP_VERSION__: JSON.stringify(pkg.version)
   },
   server: {
-    proxy: {
-      '/background': {
-        target: 'https://cn.bing.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/background/, '')
-      },
-      '/hitokoto': {
-        target: 'https://v1.hitokoto.cn',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/hitokoto/, '')
-      }
-    },
     host: '0.0.0.0'
   },
   resolve: {
