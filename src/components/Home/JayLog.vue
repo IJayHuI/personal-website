0ea173419d16bbc84038f1ce02367ceb28e778ba
IJayHuI <2949763264@qq.com>
2025-11-18 18:52:19 +0800

    update: 移除了时间，只展示日期

--- src/components/Home/JayLog.vue
+++ src/components/Home/JayLog.vue
@@ -18,7 +18,7 @@
     <n-button secondary class="whitespace-normal !text-wrap" size="large" tag="a" target="_blank" href="https://github.com/IJayHuI/personal-website/releases/">前往 Github Realeases 查看更详细信息</n-button>
     <n-timeline>
       <template v-for="item in log.datas" :key="item.id">
-        <n-timeline-item :type="item.prerelease ? 'warning' : 'success'" v-if="!item.draft" :time="item.published_at.replace('T', ' ').replace('Z', '')" :title="`版本：${item.tag_name.slice(1)}`" />
+        <n-timeline-item :type="item.prerelease ? 'warning' : 'success'" v-if="!item.draft" :time="new Date(item.published_at).toLocaleDateString('zh-cn')" :title="`版本：${item.tag_name.slice(1)}`" />
       </template>
     </n-timeline>
   </n-scrollbar>
