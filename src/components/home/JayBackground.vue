5cdf8a831f4f71bf5524f985f014bcb3d2196757
IJayHuI <2949763264@qq.com>
2026-03-04 13:42:15 +0800

    refactor: background module

---
+++ src/components/home/JayBackground.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  const props = withDefaults(
+    defineProps<{
+      isMobile: boolean
+      backgroundSrc: string
+      backgroundBlur?: number
+      backgroundBrightness?: number
+      backgroundScale?: number
+    }>(),
+    {
+      isMobile: false,
+      backgroundSrc: '',
+      backgroundBlur: 20,
+      backgroundBrightness: 70,
+      backgroundScale: 130
+    }
+  )
+</script>
+<template>
+  <img
+    v-if="!props.isMobile"
+    class="fixed w-full h-full object-cover bg-cover -z-9"
+    :style="{
+      filter: `blur(${props.backgroundBlur}px) brightness(${props.backgroundBrightness}%)`,
+      transform: `scale(${props.backgroundScale}%) translate(var(--move-x, 0px), var(--move-y, 0px))`
+    }"
+    :src="props.backgroundSrc"
+    alt="背景图片" />
+  <img
+    v-else
+    class="fixed w-full h-full object-cover bg-cover -z-9"
+    :style="{
+      filter: `blur(${props.backgroundBlur}px) brightness(${props.backgroundBrightness}%)`
+    }"
+    :src="props.backgroundSrc"
+    alt="背景图片" />
+</template>
---
+++ src/containers/JayBackground.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+  import { onBeforeMount, onMounted, onUnmounted } from 'vue'
+  import { useMessage } from 'naive-ui'
+
+  import JayBackground from '../components/home/JayBackground.vue'
+
+  import { useStores } from '../stores'
+
+  import { getBackground, type GetBackgroundResult } from '../lib/background'
+
+  const { general, home } = useStores()
+  const message = useMessage()
+  const strength = 20
+
+  const handleMouseMove = (e: MouseEvent) => {
+    const { innerWidth, innerHeight } = window
+    const x = (e.clientX / innerWidth - 0.5) * 2
+    const y = (e.clientY / innerHeight - 0.5) * 2
+    document.documentElement.style.setProperty('--move-x', `${x * strength}px`)
+    document.documentElement.style.setProperty('--move-y', `${y * strength}px`)
+  }
+
+  onBeforeMount(() => {
+    if (!home.needGetBackground) return
+    general.loadingEventAdd()
+    getBackground(home.backgroundMode)
+      .then((response: GetBackgroundResult) => {
+        message.success(response.message)
+        home.setBackgroundSrc(response.img)
+        home.setBackgroundMode(response.mode)
+      })
+      .finally(() => {
+        general.loadingEventSubtract()
+        home.setNeedGetBackground(false)
+      })
+  })
+  onMounted(() => {
+    if (general.isMobile) return
+    window.addEventListener('mousemove', handleMouseMove)
+  })
+  onUnmounted(() => {
+    window.removeEventListener('mousemove', handleMouseMove)
+    document.documentElement.style.removeProperty('--move-x')
+    document.documentElement.style.removeProperty('--move-y')
+  })
+</script>
+<template>
+  <jay-background :is-mobile="general.isMobile" :background-src="home.backgroundSrc" />
+</template>
