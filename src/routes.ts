0db689b1aced58c4b7f94bbfa3f6b44fa2557a07
IJayHuI <2949763264@qq.com>
2026-03-04 13:36:53 +0800

    refactor: router init

---
+++ src/routes.ts
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: () => import('./containers/JayHome.vue')
+  },
+  {
+    path: '/navigate',
+    name: 'Navigate',
+    component: () => import('./containers/JayNavigate.vue')
+  },
+  {
+    path: '/project',
+    name: 'Project',
+    component: () => import('./containers/JayProject.vue')
+  }
+]
+
+// 创建路由
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+export default router
