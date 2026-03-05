6dbd0a928b3e2ada4a3de224ac73b2e9d897904f
IJayHuI <2949763264@qq.com>
2026-03-05 09:07:28 +0800

    refactor: footer init

---
+++ src/components/JayFooter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  const props = withDefaults(
+    defineProps<{
+      appVersion?: string
+    }>(),
+    {
+      appVersion: ''
+    }
+  )
+</script>
+<template>
+  <span>&copy; 2024-2026 JayHu.</span>
+  <span><a class="hover:underline" target="_blank" href="https://beian.miit.gov.cn">浙ICP备2024102465号</a></span>
+  <span>Version: {{ appVersion }}</span>
+</template>
---
+++ src/containers/JayFooter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import JayFooter from '../components/JayFooter.vue'
+
+  const appVersion = __APP_VERSION__
+</script>
+
+<template>
+  <jay-footer :app-version="appVersion" />
+</template>
