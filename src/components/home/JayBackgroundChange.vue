d3b67eec831242f711c4bcc222b3e924d8c12fac
IJayHuI <2949763264@qq.com>
2026-03-07 09:07:53 +0800

    refactor: settings init

---
+++ src/components/home/JayBackgroundChange.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import { AutoAwesomeRound, AutorenewRound } from '@vicons/material'
+
+  import type { BackgroundMode } from '../../stores'
+
+  const props = withDefaults(
+    defineProps<{
+      backgroundMode?: BackgroundMode
+      setBackgroundMode?: (mode: BackgroundMode) => void
+      randomBackground?: () => void
+    }>(),
+    {
+      backgroundMode: 'bing',
+      setBackgroundMode: () => {},
+      randomBackground: () => {}
+    }
+  )
+</script>
+<template>
+  <n-card class="interaction" size="medium" @touchstart="" content-class="flex justify-center items-center gap-5">
+    <n-icon size="40"><auto-awesome-round /></n-icon>
+    <div class="flex gap-2">
+      <n-button :class="props.backgroundMode === 'bing' ? '!outline' : ''" @click="props.setBackgroundMode('bing')" secondary round>bing壁纸</n-button>
+      <n-button :class="props.backgroundMode === 'local' ? '!outline' : ''" @click="props.setBackgroundMode('local')" secondary round>系统壁纸</n-button>
+    </div>
+    <n-button v-if="props.backgroundMode === 'local'" @click="props.randomBackground()" quaternary circle size="large">
+      <template #icon>
+        <n-icon size="25"><autorenew-round /></n-icon>
+      </template>
+    </n-button>
+  </n-card>
+</template>
---
+++ src/components/home/JayThemeChange.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import { ContrastRound } from '@vicons/material'
+
+  import type { ThemeMode } from '../../stores'
+
+  const props = withDefaults(
+    defineProps<{
+      themeMode?: ThemeMode
+      setThemeMode?: (mode: ThemeMode) => void
+    }>(),
+    {
+      themeMode: 'system',
+      setThemeMode: () => {}
+    }
+  )
+</script>
+<template>
+  <n-card class="interaction" size="medium" @touchstart="" content-class="flex justify-center items-center gap-5">
+    <n-icon size="40"><contrast-round /></n-icon>
+    <div class="flex gap-2">
+      <n-button :class="props.themeMode === 'dark' ? '!outline' : ''" @click="props.setThemeMode('dark')" secondary round>暗色</n-button>
+      <n-button :class="props.themeMode === 'system' ? '!outline' : ''" @click="props.setThemeMode('system')" secondary round>跟随系统</n-button>
+      <n-button :class="props.themeMode === 'light' ? '!outline' : ''" @click="props.setThemeMode('light')" secondary round>浅色</n-button>
+    </div>
+  </n-card>
+</template>
