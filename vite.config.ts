0af6423d6cb42d319790a7fec24caddc962f4a88
IJayHuI <2949763264@qq.com>
2026-03-07 09:00:45 +0800

    chore: 添加 svg loader 包

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import svgLoader from 'vite-svg-loader'
 import tailwindcss from '@tailwindcss/vite'
 import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue(), tailwindcss(), svgLoader()],
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version)
   },
