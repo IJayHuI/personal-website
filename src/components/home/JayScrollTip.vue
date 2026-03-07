8c0be5201d39f4a9a8375ce87b60b8bba30d90dd
IJayHuI <2949763264@qq.com>
2026-03-07 09:07:18 +0800

    refactor: scroll tip init

---
+++ src/components/home/JayScrollTip.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  import { ArrowUpwardRound } from '@vicons/material'
+
+  const props = withDefaults(
+    defineProps<{
+      size?: number
+    }>(),
+    {
+      size: 36
+    }
+  )
+</script>
+<template>
+  <div class="w-full flex justify-center items-center mt-5 mb-5">
+    <n-icon :size="props.size" style="animation: floatY 8s infinite">
+      <arrow-upward-round />
+    </n-icon>
+  </div>
+</template>
+<style>
+  @keyframes floatY {
+    0% {
+      transform: translateY(0);
+    }
+    20% {
+      transform: translateY(-20px);
+    }
+    40% {
+      transform: translateY(0);
+    }
+    /* 60%~100% 保持静止，实现“停几秒”的效果 */
+    100% {
+      transform: translateY(0);
+    }
+  }
+</style>
