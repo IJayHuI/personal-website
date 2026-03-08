0136201e57cb7fa0084dac1f1f46af706cf4022d
IJayHuI <2949763264@qq.com>
2026-03-08 15:16:37 +0800

    refactor: tech list init

---
+++ src/components/home/JayTechList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  const props = withDefaults(
+    defineProps<{
+      techList: {
+        href: string
+        name: string
+      }[]
+    }>(),
+    {
+      techList: () => []
+    }
+  )
+</script>
+
+<template>
+  <n-button round secondary size="small" v-for="item in props.techList" tag="a" :href="item.href" target="_blank">
+    {{ item.name }}
+  </n-button>
+</template>
