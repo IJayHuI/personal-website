a954ff3adeaa802c35e835306f417f3da50d3249
IJayHuI <2949763264@qq.com>
2026-03-13 01:29:53 +0800

    feat: 全局导入组件

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,31 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-import { create, NConfigProvider, NLayout, NLayoutContent, NLayoutFooter, NLayoutSider, NCard, NIcon, NMenu, NCollapse, NCollapseItem, NButton, NDrawer, NDrawerContent, NSpin, NSpace, NMessageProvider, NScrollbar, NCarousel } from 'naive-ui'
+import {
+  create,
+  NConfigProvider,
+  NLayout,
+  NLayoutHeader,
+  NLayoutContent,
+  NLayoutFooter,
+  NLayoutSider,
+  NCard,
+  NIcon,
+  NMenu,
+  NCollapse,
+  NCollapseItem,
+  NButton,
+  NDrawer,
+  NDrawerContent,
+  NSpin,
+  NSpace,
+  NMessageProvider,
+  NScrollbar,
+  NCarousel
+} from 'naive-ui'
 import 'vfonts/Lato.css'
 const naive = create({
-  components: [NConfigProvider, NLayout, NLayoutContent, NLayoutFooter, NLayoutSider, NCard, NIcon, NMenu, NCollapse, NCollapseItem, NButton, NDrawer, NDrawerContent, NSpin, NSpace, NMessageProvider, NScrollbar, NCarousel]
+  components: [NConfigProvider, NLayout, NLayoutHeader, NLayoutContent, NLayoutFooter, NLayoutSider, NCard, NIcon, NMenu, NCollapse, NCollapseItem, NButton, NDrawer, NDrawerContent, NSpin, NSpace, NMessageProvider, NScrollbar, NCarousel]
 })
 
 import { createPinia } from 'pinia'
