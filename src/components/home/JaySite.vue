7cba0377d538eb6308c927dc4fa0c1dd237bde37
IJayHuI <2949763264@qq.com>
2026-03-27 01:01:13 +0800

    fix: bug fix

--- src/components/home/JaySite.vue
+++ src/components/home/JaySite.vue
@@ -25,28 +25,40 @@
 </script>
 <template>
   <n-card v-if="!props.isMobile" size="medium" title="站点" content-class="w-full grid gap-2 grid-cols-[repeat(auto-fill,minmax(200px,1fr))]">
-    <template v-for="item in props.sites" :key="item.router || item.link">
-      <component :is="item.type === 'router' ? 'router-link' : 'a'" :to="item.type === 'router' ? item.router : undefined" :href="item.type === 'link' ? item.link : undefined" target="_blank">
-        <n-card v-interaction class="w-full h-full" size="medium" :content-class="`flex justify-center items-center gap-2 flex-row`">
-          <n-icon :size="35">
-            <component :is="renderIcon(item.icon)" />
-          </n-icon>
-          <p class="text-xl font-bold">{{ item.name }}</p>
+    <template v-for="site in props.sites" :key="site.router">
+      <router-link v-if="site.type === 'router' && site.router" :to="site.router">
+        <n-card v-interaction size="medium" content-class="flex justify-center items-center gap-2 flex-row">
+          <n-icon :size="30"><component :is="renderIcon(site.icon)" /></n-icon>
+          <p class="text-xl font-bold">{{ site.name }}</p>
         </n-card>
-      </component>
+      </router-link>
+    </template>
+    <template v-for="link in props.sites" :key="link.link">
+      <a v-if="link.type === 'link' && link.link" :href="link.link" target="_blank">
+        <n-card v-interaction class="w-full h-full" size="medium" content-class="flex justify-center items-center gap-2 flex-row">
+          <n-icon :size="30"><component :is="renderIcon(link.icon)" /></n-icon>
+          <p class="text-xl font-bold">{{ link.name }}</p>
+        </n-card>
+      </a>
     </template>
   </n-card>
   <n-card v-else size="small" title="站点">
     <n-carousel show-arrow :slides-per-view="3" :space-between="8" class="pb-12">
-      <template v-for="item in props.sites" :key="item.router || item.link">
-        <component :is="item.type === 'router' ? 'router-link' : 'a'" :to="item.type === 'router' ? item.router : undefined" :href="item.type === 'link' ? item.link : undefined" target="_blank">
-          <n-card v-interaction class="w-full h-full" size="small" :content-class="`flex justify-center items-center flex-col`">
-            <n-icon :size="27">
-              <component :is="renderIcon(item.icon)" />
-            </n-icon>
-            <p class="text-base">{{ item.name }}</p>
+      <template v-for="site in props.sites" :key="site.router">
+        <router-link v-if="site.type === 'router' && site.router" :to="site.router">
+          <n-card v-interaction size="small" content-class="flex justify-center items-center gap-2 flex-row">
+            <n-icon :size="27"><component :is="renderIcon(site.icon)" /></n-icon>
+            <p class="text-base">{{ site.name }}</p>
           </n-card>
-        </component>
+        </router-link>
+      </template>
+      <template v-for="link in props.sites" :key="link.link">
+        <a v-if="link.type === 'link' && link.link" :href="link.link" target="_blank">
+          <n-card v-interaction class="w-full h-full" size="small" content-class="flex justify-center items-center gap-2 flex-col">
+            <n-icon :size="27"><component :is="renderIcon(link.icon)" /></n-icon>
+            <p class="text-base">{{ link.name }}</p>
+          </n-card>
+        </a>
       </template>
     </n-carousel>
   </n-card>
