db076ac19b034711b265771a6c80a7f8a029bf92
JayHu <2949763264@qq.com>
2026-09-07 13:31:27 +0800

    refactor: 迁移 blog 模块组件（空壳）

---
+++ src/components/blog/JayArticleList.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+// 博客文章列表（空壳，预留扩展点）
+</script>
+
+<template>ArticleList</template>
--- src/views/blog/BlogView.vue
+++ src/views/blog/BlogView.vue
@@ -1,21 +1,7 @@
 <script setup lang="ts">
-// 阶段 2：占位页面，阶段 4 迁移 blog 模块组件
+import JayBlog from '../../components/blog/JayBlog.vue'
 </script>
 
 <template>
-  <div class="view-placeholder">
-    <h1>Blog</h1>
-    <p>博客占位，等待阶段 4 迁移组件。</p>
-  </div>
+  <jay-blog />
 </template>
-
-<style scoped>
-.view-placeholder {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 60vh;
-  color: #666;
-}
-</style>
